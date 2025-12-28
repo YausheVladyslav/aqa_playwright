7ba8026ec2ededacfe64619bede2090679f221bf
VladyslavYaushev <i.vlad.yaushev@gmail.com>
2025-12-28 14:25:06 +0200

    wrote some tests for registration form

--- playwright.config.js
+++ playwright.config.js
@@ -14,8 +14,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-  globalSetup: require.resolve('./global-setup'),
-  globalTeardown: require.resolve('./global-teardown'),
+  // globalSetup: require.resolve('./global-setup'),
+  // globalTeardown: require.resolve('./global-teardown'),
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   // testMatch: /.*\.spec\.js/,
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -45,7 +45,7 @@ export default defineConfig({
     // viewport: { width: 1280, height: 720 },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
     // video: 'on-first-retry',
     screenshot: 'only-on-failure',
   },
@@ -56,30 +56,30 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'regression',
-      grepInvert: /@ui/,
-      // dependencies: ['chromium', 'firefox', 'webkit', 'setup'],
-      use: {
-        ...devices['Desktop Chrome'],
-        // viewport: { width: 1920, height: 1080 },
-        // trace: 'on-first-retry',
-        // baseURL: 'https://qauto.forstudy.space/'
-      },
-    },
+    // {
+    //   name: 'regression',
+    //   grepInvert: /@ui/,
+    //   // dependencies: ['chromium', 'firefox', 'webkit', 'setup'],
+    //   use: {
+    //     ...devices['Desktop Chrome'],
+    //     // viewport: { width: 1920, height: 1080 },
+    //     // trace: 'on-first-retry',
+    //     // baseURL: 'https://qauto.forstudy.space/'
+    //   },
+    // },
 
-    {
-      name: 'smoke',
-      grep: /@smoke/,
-      // testMatch: /tests\/smoke\/.*\.spec\.js/,
-      use: { ...devices['Desktop Chrome'],
-        trace: 'on-first-retry',
-        screenshot: {
-          fullPage: true,
-          mode: 'only-on-failure',
-        }
-       },
-    },
+    // {
+    //   name: 'smoke',
+    //   grep: /@smoke/,
+    //   // testMatch: /tests\/smoke\/.*\.spec\.js/,
+    //   use: { ...devices['Desktop Chrome'],
+    //     trace: 'on-first-retry',
+    //     screenshot: {
+    //       fullPage: true,
+    //       mode: 'only-on-failure',
+    //     }
+    //    },
+    // },
 
     // {
     //   name: 'chromium',
---
+++ tests/registrationForm.spec.js
@@ -0,0 +1,157 @@
+import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
+
+test.describe("Registration form", () => {
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/');
+        await page.locator(".btn-primary").click();
+        const signUpModal = page.locator(".modal-content")
+        await expect(signUpModal).toBeVisible();
+    })
+
+
+    test("Sign up with valid data should be successful", async ({ page }) => {
+
+        const password = `Testqa${faker.number.int({ min: 100, max: 1000 })}`
+        const userData = {
+            "name": faker.person.firstName(),
+            "lastName": faker.person.lastName(),
+            "email": faker.internet.email(),
+            "password": password,
+            "repeatPassword": password
+        }
+
+        const signUpModal = page.locator(".modal-content")
+        const nameField = signUpModal.locator("#signupName")
+        const lastNameField = signUpModal.locator("#signupLastName")
+        const emailField = signUpModal.locator("#signupEmail")
+        const passwordField = signUpModal.locator("#signupPassword")
+        const repeatPasswordField = signUpModal.locator("#signupRepeatPassword")
+        const registerButton = signUpModal.getByRole("button", { name: "Register" })
+
+        await nameField.fill(userData.name);
+        await lastNameField.fill(userData.lastName);
+        await emailField.fill(userData.email);
+        await passwordField.fill(userData.password);
+        await repeatPasswordField.fill(userData.repeatPassword);
+
+        expect(nameField).toHaveValue(userData.name);
+        expect(lastNameField).toHaveValue(userData.lastName);
+        expect(emailField).toHaveValue(userData.email);
+        expect(passwordField).toHaveValue(userData.password);
+        expect(repeatPasswordField).toHaveValue(userData.repeatPassword);
+        expect(registerButton).toBeEnabled();
+
+        registerButton.click();
+
+        const garageEmptystate = page.locator(".panel-empty_message")
+        await expect(garageEmptystate).toBeVisible();
+        await expect(garageEmptystate).toHaveText("You don’t have any cars in your garage");
+    })
+
+    test("Empty 'Name' field should show an error message", async ({ page }) => {
+
+        const signUpModal = page.locator(".modal-content")
+        const nameField = signUpModal.locator("#signupName")
+
+        await nameField.focus();
+        await nameField.blur();
+
+        const errorMessage = signUpModal.locator(".invalid-feedback")
+
+        expect(nameField).toHaveCSS('border-color', 'rgb(220, 53, 69)');
+        expect(errorMessage).toBeVisible();
+        expect(errorMessage).toHaveText('Name required');
+
+    })
+
+    test("Invalid Name with 1 character should show error message", async ({ page }) => {
+        const name = 'A'
+        const signUpModal = page.locator(".modal-content")
+        const nameField = signUpModal.locator("#signupName")
+        await nameField.fill(name);
+        await nameField.blur();
+
+        const errorMessage = signUpModal.locator(".invalid-feedback")
+
+        expect(nameField).toHaveValue(name);
+        expect(nameField).toHaveCSS('border-color', 'rgb(220, 53, 69)');
+        expect(errorMessage).toBeVisible();
+        expect(errorMessage).toHaveText('Name has to be from 2 to 20 characters long');
+
+    })
+
+    test("Invalid Name with 21 character should show error message", async ({ page }) => {
+        const name = 'qwertyuiopasdfghjklzx' //21 characters
+        const signUpModal = page.locator(".modal-content")
+        const nameField = signUpModal.locator("#signupName")
+        await nameField.fill(name);
+        await nameField.blur();
+
+        const errorMessage = signUpModal.locator(".invalid-feedback")
+
+        expect(nameField).toHaveValue(name);
+        expect(nameField).toHaveCSS('border-color', 'rgb(220, 53, 69)');
+        expect(errorMessage).toBeVisible();
+        expect(errorMessage).toHaveText('Name has to be from 2 to 20 characters long');
+
+    })
+
+    test("Valid email with subdomain should be accepted", async ({ page }) => {
+        const email = 'user123@email.co.uk'
+        const signUpModal = page.locator(".modal-content")
+        const emailField = signUpModal.locator("#signupEmail")
+        await emailField.fill(email);
+        await emailField.blur();
+
+        const errorMessage = signUpModal.locator(".invalid-feedback")
+
+        expect(emailField).toHaveValue(email);
+        expect(errorMessage).not.toBeVisible();
+
+    })
+
+    test("Reloading page should reset all fields", async ({ page }) => {
+
+        const password = `Testqa${faker.number.int({ min: 100, max: 1000 })}`
+        const userData = {
+            "name": faker.person.firstName(),
+            "lastName": faker.person.lastName(),
+            "email": faker.internet.email(),
+            "password": password,
+            "repeatPassword": password
+        }
+
+        const signUpModal = page.locator(".modal-content")
+        const nameField = signUpModal.locator("#signupName")
+        const lastNameField = signUpModal.locator("#signupLastName")
+        const emailField = signUpModal.locator("#signupEmail")
+        const passwordField = signUpModal.locator("#signupPassword")
+        const repeatPasswordField = signUpModal.locator("#signupRepeatPassword")
+
+        await nameField.fill(userData.name);
+        await lastNameField.fill(userData.lastName);
+        await emailField.fill(userData.email);
+        await passwordField.fill(userData.password);
+        await repeatPasswordField.fill(userData.repeatPassword);
+
+        expect(nameField).toHaveValue(userData.name);
+        expect(lastNameField).toHaveValue(userData.lastName);
+        expect(emailField).toHaveValue(userData.email);
+        expect(passwordField).toHaveValue(userData.password);
+        expect(repeatPasswordField).toHaveValue(userData.repeatPassword);
+
+        await page.reload();
+
+        expect(signUpModal).not.toBeVisible();
+
+        await page.locator(".btn-primary").click();
+        expect(nameField).toBeEmpty();
+        expect(lastNameField).toBeEmpty();
+        expect(emailField).toBeEmpty();
+        expect(passwordField).toBeEmpty();
+        expect(repeatPasswordField).toBeEmpty();
+    })
+
+})
