45e5a3a85381b87ff1ef5d48e479421211833160
VladyslavYaushev <i.vlad.yaushev@gmail.com>
2026-01-08 00:56:19 +0200

    update config

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const config = {
+const config = {
     baseURL: process.env.BASE_URL,
     httpCredentials: {
-        username: process.env.HTTP_CREDENTIALS_USERNAME+'',
-        password: process.env.HTTP_CREDENTIALS_PASSWORD+'',
+        username: process.env.HTTP_CREDENTIALS_USERNAME + '',
+        password: process.env.HTTP_CREDENTIALS_PASSWORD + '',
     },
 };
 
